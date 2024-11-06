4bef1bca8e7408cdacba9888bc2c815599ef16e6
Dober09 <lebohanglehutjo@gmail.com>
2024-11-06 21:27:29 +0200

    added mock data json and model for products

---
+++ WorldMap/Model/Product.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Text.Json.Serialization;
+
+
+namespace WorldMap.Model
+{
+    public class Product
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("category")]
+        public string Category { get; set; }
+
+        [JsonPropertyName("price")]
+        public decimal Price { get; set; }
+        [JsonPropertyName("weight")]
+        public string Weight { get; set; }
+        [JsonPropertyName("manufacturer")]
+        public string Manufacturer { get; set; }
+    }
+
+    public class BarcodeData
+    {
+        [JsonPropertyName("barcode")]
+        public string Barcode { get; set; }
+        [JsonPropertyName("format")]
+        public string Format { get; set; }
+        [JsonPropertyName("product")]
+        public Product Product { get; set; }
+    }
+
+}
